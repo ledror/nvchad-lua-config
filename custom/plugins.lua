956034e7ed66a8faf25ee8e9bccf87b32b958c2f
ledror <drorelmakias@gmail.com>
2023-09-01 18:49:07 +0300

    added all files

---
+++ custom/plugins.lua
@@ -0,0 +1,42 @@
+local plugins = {
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+      },
+    },
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = "go",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- defaults
+        "vim",
+        "lua",
+
+        -- high level
+        "python",
+        "go",
+
+        -- low level
+        "c",
+      },
+    },
+  },
+}
+return plugins
